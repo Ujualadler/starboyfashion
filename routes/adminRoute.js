d7bee654503924f09bbdef18c7cba479601961db
ujualk <ujualk2000@gmail.com>
2023-04-27 16:13:20 +0530

    multer

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,path.join(__dirname,'../public/img/temp'))
+        cb(null,path.join(__dirname,'../public/img'))
     },
     filename:function(req,file,cb){
         cb(null,Date.now()+''+file.originalname)
